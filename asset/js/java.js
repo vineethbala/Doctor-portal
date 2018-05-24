7ebe13722443996a19fd9df61d64d8273d2c084e
vineethbala <39599755+vineethbala@users.noreply.github.com>
2018-05-25 00:10:58 +0530

    Add files via upload

---
+++ asset/js/java.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+    // Activate Carousel with a specified interval
+    
+
+        $('.open-popup-link').magnificPopup({
+  type:'inline',
+  midClick: true // Allow opening popup on middle mouse click. Always set it to true if you don't provide alternative source in href.
+});
+
+
+
+
+    
+});
